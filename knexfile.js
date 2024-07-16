ec8fb3461c1084ff64b62d75bfd5840b3d17cdcc
ALEXITOSOTO <luisalexander.sotosegovia.3@gmail.com>
2024-07-16 08:44:38 -0500

    v1

--- knexfile.js
+++ knexfile.js
@@ -9,8 +9,8 @@ module.exports = {
     client: 'mysql2',
     connection: {
       host: '127.0.0.1',
-      user: 'root',
-      password: '',
+      user: 'api_user',
+      password: '123456',
       database: 'cloud'
     },
     migrations: {
